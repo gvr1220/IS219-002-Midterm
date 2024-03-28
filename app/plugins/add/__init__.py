be25923e940d1be868799fb0ba664cb6aaf4a899
gvr1220 <gv225@njit.edu>
2024-03-28 13:16:17 -0400

    update

---
+++ app/plugins/add/__init__.py
@@ -0,0 +1,8 @@
+from app.commands.operation_command import OperationCommand
+from calculator.operations import add
+import logging
+
+class AddCommand(OperationCommand):
+    def __init__(self):
+        super().__init__(add)
+        self.logger = logging.getLogger(__name__)
